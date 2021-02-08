7846e49ec743058dee0a41907047d0197d7b0d32
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-02-08 15:24:00 -0800

    add recipe section

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import './App.css';
 import ChocolateHeader from './header.js';
 import ImageComponent from './image.js';
 import PizzaHeading from './pizza-heading.js';
+import ChocolateRecipe from './recipe.js';
 
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <ChocolateHeader/>
-        <ImageComponent/>
-        <PizzaHeading/>
+        <ChocolateHeader />
+        <ImageComponent />
+        <PizzaHeading />
+        <ChocolateRecipe />
       </div>
     );
   }
---
+++ src/recipe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class ChocolateRecipe extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat dolor quam, in consequat lectus pretium eget. Nulla sollicitudin ante leo, sit amet malesuada dolor interdum commodo. Aenean luctus, lectus sollicitudin euismod fermentum, mi quam posuere diam, ac finibus neque sapien vel justo. Nulla eu lacus nibh. Maecenas consectetur dignissim augue quis auctor. Etiam fringilla sit amet ipsum ut dictum. Pellentesque mattis, quam ac egestas finibus, ipsum lectus porta lacus, et sagittis odio velit in felis. Integer ac fermentum justo. Vivamus quis est elit. Nunc vitae massa id orci elementum porta. Vestibulum a tortor varius tortor gravida fermentum. Pellentesque hendrerit porttitor arcu id malesuada.</p>
+
+                <p>Donec eleifend enim velit. Nulla non ex quis nisi blandit condimentum. Nullam eget diam a elit gravida aliquet. Aliquam sed dui sem. Duis blandit mauris vel odio eleifend, eu scelerisque lorem blandit. Mauris finibus dolor ligula, et consequat eros luctus vel. Vivamus mauris sapien, lobortis sit amet aliquet a, pharetra feugiat felis.</p>
+
+                <p>Aliquam vitae lobortis orci. In augue odio, condimentum a efficitur at, ultricies dictum mi. Nunc ullamcorper a sem a dignissim. Nullam malesuada dui luctus magna volutpat, at euismod neque consectetur. Morbi id purus et leo maximus laoreet. Aenean massa felis, facilisis et nunc nec, fermentum laoreet arcu. Vestibulum neque augue, semper et lacinia nec, lobortis eget turpis. Cras velit purus, euismod eu diam sed, iaculis varius ligula. Ut vitae accumsan justo.</p>
+            </div>
+        );
+    }
+}
