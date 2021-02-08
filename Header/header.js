e4ee9e442c4cca06508bae97dcfab24d537f9543
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-02-08 14:52:13 -0800

    finish header component

--- Header/header.js
+++ Header/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavButton } from './Button/button.js';
 
 export class ChocolateHeader extends React.Component {
     render() {
@@ -6,6 +7,14 @@ export class ChocolateHeader extends React.Component {
             <img alt="logo" src="logo.png"></img>
             <h1>Delicious</h1>
             <h3>THE BEST FOOD ON THE WEB</h3>
+            <NavButton image = "fb-icon.png"/>
+            <NavButton image = "twit-icon.png"/>
+            <NavButton image = "gp-icon.png"/>
+            <NavButton image = "insta-icon.png"/>
+            <NavButton image = "flic-icon.png"/>
+            <NavButton image = "pint-icon.png"/>
+            <NavButton image = "rss-icon.png"/>
+            <NavButton image = "mail-icon.png"/>
 
         </div>
     }
